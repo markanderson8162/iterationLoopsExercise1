1c411738441beef4631777e501358776e52fa3db
Mark Anderson <markanderson8162@gmail.com>
2021-06-14 21:26:43 -0500

    finished first loops exercise

---
+++ iterationLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace iterationLoops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 15; i++)
+			{
+				Console.WriteLine($"{i}");
+			}
+
+			for (int i = 0; i <15; i++)
+			{
+				Console.WriteLine(i);
+			}
+
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine(i);
+			}
+
+			
+		}
+	}
+}
